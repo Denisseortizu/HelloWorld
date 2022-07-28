481f2a683706d0619723839a2628c8eb7e2ce33c
Denisseortizu <denisseortizu@gmail.com>
2022-07-28 16:41:20 -0500

    first commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+/* This is my first C# project 
+   Here we have a simple console application. 
+   My main goal is to lear the basic syntaxis.
+*/
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//We can manage to write on the console with
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            var currentDate = DateTime.Now;
+            Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
+
+
